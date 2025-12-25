8a92815a44d93776c40b97566ec3b015cc6914e4
michaelverapro <michaelvera.pro@gmail.com>
2025-12-25 00:59:55 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,13 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
+    },
+    plugins: [react()],
+    build: {               
+      target: "esnext",    
+    },                      
+    define: {
+
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
